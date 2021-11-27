a23e02969d3609fd5443fa082ff13eb244aa3468
ErgiAr <93042906+ErgiAr@users.noreply.github.com>
2021-11-27 13:21:48 +0700

    Recycler View
    Data Teman

---
+++ app/src/main/java/com/ergi/ergiarr/MyFriend.kt
@@ -0,0 +1,9 @@
+package com.ergi.ergiarr
+
+class MyFriend (
+    val nama : String,
+    val jkel : String,
+    val email : String,
+    val telp : String,
+    val alamat : String
+)
--- app/src/main/java/com/ergi/ergiarr/MyFriendsFragment.kt
+++ app/src/main/java/com/ergi/ergiarr/MyFriendsFragment.kt
@@ -6,8 +6,53 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_my_friends.*
 
 class MyFriendsFragment : Fragment() {
+    lateinit var listTeman:ArrayList<MyFriend>
+
+    private fun simulasiDataTeman(){
+        listTeman= ArrayList()
+        listTeman.add(
+            MyFriend("Ergi Alifio Rifero", "Laki-laki", "ergi_20310022@stimata.ac.id",
+            "085258022025", "Klayatan")
+        )
+        listTeman.add(
+            MyFriend("Alief Affansyah", "Laki-laki", "alief_20310023@stimata.ac.id",
+            "089654034070", "Pandan Landung")
+        )
+        listTeman.add(
+            MyFriend("Bobby Penaloza", "Laki-laki", "bobby_20310019@stimata.ac.id",
+            "08887085140", "Jodipan")
+        )
+        listTeman.add(
+            MyFriend("Moch Rizal Hermawan", "Laki-laki", "moch_20310012@stimata.ac.id",
+            "085895875364", "Blitar")
+        )
+        listTeman.add(
+            MyFriend("Denisa Mei Linda Sari", "Perempuan", "denisa_20310018@stimata.ac.id",
+            "085888043162", "Blitar")
+        )
+        listTeman.add(
+            MyFriend("Livia Dita Eka Puspita", "Perempuan", "livia_20310017@stimata.ac.id",
+            "088234039767", "Sawojajar")
+        )
+        listTeman.add(
+            MyFriend("Nur Intan Maghfiroh", "Perempuan", "nur_20310021@stimata.ac.id",
+            "08871294510", "Gampingan")
+        )
+        listTeman.add(
+            MyFriend("Izza Syafia Almaliki", "Perempuan", "izza_20310006@stimata.ac.id",
+            "081331093499", "Panjen")
+        )
+    }
+    private fun tampilTeman(){
+        rv_listMyfriends.layoutManager=LinearLayoutManager(activity)
+        rv_listMyfriends.adapter=MyFriendAdapter(activity!!, listTeman)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -22,5 +67,16 @@ class MyFriendsFragment : Fragment() {
 
     override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView(){
+        simulasiDataTeman()
+        tampilTeman()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this.clearFindViewByIdCache()
     }
 }
